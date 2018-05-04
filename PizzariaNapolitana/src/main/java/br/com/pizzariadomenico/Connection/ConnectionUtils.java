017ef5ff9302cae48dbade4f7953f23276a0aef3
jonat <jonat@DESKTOP-L6MFD45>
2018-05-04 10:51:46 -0300

    Alteração de conexão para novo banco de dados.

--- PizzariaNapolitana/src/main/java/br/com/pizzariadomenico/Connection/ConnectionUtils.java
+++ PizzariaNapolitana/src/main/java/br/com/pizzariadomenico/Connection/ConnectionUtils.java
@@ -22,10 +22,10 @@ public class ConnectionUtils {
             Class.forName(driverName);
             
             //Configurando a conexao com o banco de dados
-            String serveName = "napolitana01.mysql.uhserver.com"; // Caminho do servidor do BD
-            String mydatabase = "napolitana01"; // Nome do banco de dados
+            String serveName = "napolitana02.mysql.uhserver.com"; // Caminho do servidor do BD
+            String mydatabase = "napolitana02"; // Nome do banco de dados
             String url = "jdbc:mysql://" + serveName + "/" + mydatabase;
-            String username = "napolitana01"; // Nome de usuario
+            String username = "napolitana02"; // Nome de usuario
             String password = "br@sil20"; // Senha de acesso
             connection = DriverManager.getConnection(url, username, password);
             
